9a06e19d64c277d8e4d2927b445b8f77edbc3a03
Elgashia <erwinousy@gmail.com>
2019-01-12 12:08:18 +0900

    Add vuetify

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    sdsd
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
